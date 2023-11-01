1b91784067a4fe1becff0fa0a4737b56eced694f
MSPROHITH <rohiths819@gmail.com>
2023-11-01 08:31:45 +0530

    c

---
+++ JS/JS Folder/1-11-23.js
@@ -0,0 +1,22 @@
+// var person={
+//     name:"abc",
+//     sal:"7lpa",
+//     hobbies:["dance","singing"],
+//     add:{
+//         area:"majestic",
+//         city:"bangalore"
+//     }
+// }
+// console.log(person);
+// console.log(typeof(person));
+// var r=JSON.stringify(person);
+// console.log(r);
+// console.log(typeof(r));
+// var x=JSON.parse(r);
+// console.log(x);
+// console.log(typeof(x));
+
+//!timedelays
+function task(){
+    alert("Good Morning")
+}
